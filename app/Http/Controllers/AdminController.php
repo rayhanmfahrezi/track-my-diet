bf63016afd274cc9ae7316e459e27208ab1e0408
Faqih Madani <faqihfaqih22@gmail.com>
2023-01-10 09:30:03 +0700

    finishing

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,10 +8,11 @@ use App\Models\User;
 use App\Models\UserDiet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 class AdminController extends Controller
 
 {
-    
+
     public function foodAdmin()
     {
         $foods = Food::all();
@@ -20,23 +21,23 @@ class AdminController extends Controller
 
     public function createFood_admin(Request $request)
     {
-        $this->validate($request,[
-            'photo' => 'required|mimes:jpeg',
-            'name' => 'required|string|alpha_dash',
-            'category' => 'required|string|alpha_dash',
-            'description' => 'required|string|alpha_dash',
-            'calories' => 'required|integer',
-            'carbs' => 'required|numeric|max:6',
-            'fat' => 'required|numeric|max:6',
-            'protein' => 'required|numeric|max:6',
-            'ingredient' => 'required|string',
-            'instruction' => 'required|string',
-        ]);
+        // $this->validate($request, [
+        //     'photo' => 'required|mimes:jpeg',
+        //     'name' => 'required|string|alpha_dash',
+        //     'category' => 'required|string|alpha_dash',
+        //     'description' => 'required|string|alpha_dash',
+        //     'calories' => 'required|integer',
+        //     'carbs' => 'required|numeric',
+        //     'fat' => 'required|numeric',
+        //     'protein' => 'required|numeric',
+        //     'ingredient' => 'required|string',
+        //     'instruction' => 'required|string',
+        // ]);
 
         $food = new Food();
 
-        if($request->file('photo')){
-            $file= $request->file('photo');
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
             $filename = $file->getClientOriginalName();
             $file->move(public_path("images/$request->category"), $filename);
         }
@@ -69,8 +70,8 @@ class AdminController extends Controller
     public function update(Request $request)
     {
         $food = Food::where('id', $request->id)->first();
-        if($request->file('photo')){
-            $file= $request->file('photo');
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
             $filename = $file->getClientOriginalName();
             $file->move(public_path("images/$request->category"), $filename);
         }
